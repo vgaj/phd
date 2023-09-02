ea8e63c5d1d24e0fbf501ba1cb339b3fdf047cf3
Viru Gajanayake <viru@not.set>
2023-09-03 07:27:59 +0930

    Added a parent pom

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ sudo systemctl stop phd.service
 echo "*** Deploying..."
 sudo mkdir -p /opt/phd
 cwd=$(pwd)
-sudo rm -f /opt/phd/phonehomedetector-0.0.1-SNAPSHOT.jar
+sudo rm -f /opt/phd/*.jar
 #sudo cp target/phonehomedetector*.jar /opt/phd/
-sudo ln -s $cwd/target/phonehomedetector-0.0.1-SNAPSHOT.jar /opt/phd/phonehomedetector-0.0.1-SNAPSHOT.jar
-sudo cp src/main/resources/phd.service /etc/systemd/system/
+sudo ln -s $cwd/server/target/phd-server-0.0.1-SNAPSHOT.jar /opt/phd/phd-server-0.0.1-SNAPSHOT.jar
+sudo cp server/src/main/resources/phd.service /etc/systemd/system/
 
 echo "*** Enabling..."
 sudo systemctl daemon-reload
