2ca3b9e7461c78526c9f4567768ece6c9d4d4ead
“Viru Gajanayake” <viru@not.set>
2024-09-09 21:19:14 +0930

    Fix compilation issue

--- install_dev.sh
+++ install_dev.sh
@@ -3,15 +3,14 @@
 echo "*** Stopping..."
 sudo systemctl stop phone-home-detector.service
 
-sudo mkdir -p /usr/lib/phone-home-detector
 sudo mkdir -p /usr/share/phone-home-detector
 cwd=$(pwd)
 
 echo "*** Linking files..."
 sudo ln -f -s $cwd/server/target/phd-server.jar /usr/share/phone-home-detector/phd-server.jar
 sudo ln -f -s $cwd/cli/target/phd-cli-jar-with-dependencies.jar /usr/share/phone-home-detector/phd-cli-jar-with-dependencies.jar
-sudo ln -f -s $cwd/server/target/phone_home_detector_bpf_count.o /usr/lib/phone-home-detector/phone_home_detector_bpf_count.o
-sudo ln -f -s $cwd/server/target/phone_home_detector_bpf_pid.o /usr/lib/phone-home-detector/phone_home_detector_bpf_pid.o
+sudo ln -f -s $cwd/server/src/main/bpf/phone_home_detector_bpf_count.c /usr/share/phone-home-detector/phone_home_detector_bpf_count.c
+sudo ln -f -s $cwd/server/src/main/bpf/phone_home_detector_bpf_pid.c /usr/share/phone-home-detector/phone_home_detector_bpf_pid.c
 sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector-service-start.sh /usr/share/phone-home-detector/phone-home-detector-service-start.sh
 sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector-service-stop.sh /usr/share/phone-home-detector/phone-home-detector-service-stop.sh
 sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector /usr/bin/phone-home-detector
