63f401e3989b0efedb7c7baec97206fa48cb9e6d
“Viru Gajanayake” <viru@not.set>
2025-03-15 11:33:59 +1030

    Install web app

--- install_dev.sh
+++ install_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # MIT License
 #
-# Copyright (c) 2022-2024 Viru Gajanayake
+# Copyright (c) 2022-2025 Viru Gajanayake
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@
 # SOFTWARE.
 
 echo "*** Stopping..."
+sudo systemctl stop phone-home-detector-ui.service
 sudo systemctl stop phone-home-detector.service
 
 sudo mkdir -p /usr/share/phone-home-detector
@@ -29,6 +30,7 @@ cwd=$(pwd)
 
 echo "*** Linking files..."
 sudo ln -f -s $cwd/server/target/phd-server.jar /usr/share/phone-home-detector/phd-server.jar
+sudo ln -f -s $cwd/ui/target/phd-ui.jar /usr/share/phone-home-detector/phd-ui.jar
 sudo ln -f -s $cwd/cli/target/phd-cli-jar-with-dependencies.jar /usr/share/phone-home-detector/phd-cli-jar-with-dependencies.jar
 sudo ln -f -s $cwd/server/src/main/bpf/phone_home_detector_bpf_count.c /usr/share/phone-home-detector/phone_home_detector_bpf_count.c
 sudo ln -f -s $cwd/server/src/main/bpf/phone_home_detector_bpf_pid.c /usr/share/phone-home-detector/phone_home_detector_bpf_pid.c
@@ -36,6 +38,7 @@ sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector-service-start.s
 sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector-service-stop.sh /usr/share/phone-home-detector/phone-home-detector-service-stop.sh
 sudo ln -f -s $cwd/server/src/main/resources/phone-home-detector /usr/bin/phone-home-detector
 sudo cp -f $cwd/server/src/main/resources/phone-home-detector.service /usr/lib/systemd/system/phone-home-detector.service
+sudo cp -f $cwd/ui/src/main/resources/phone-home-detector-ui.service /usr/lib/systemd/system/phone-home-detector-ui.service
 
 echo "*** Clean up..."
 if [ -f "/usr/share/phone-home-detector/phone_home_detector_bpf_count.o" ]; then
@@ -47,5 +50,7 @@ fi
 
 echo "*** Starting..."
 sudo systemctl daemon-reload
-sudo systemctl enable phone-home-detector
+sudo systemctl enable phone-home-detector.service
+sudo systemctl enable phone-home-detector-ui.service
 sudo systemctl start phone-home-detector.service
+sudo systemctl start phone-home-detector-ui.service
