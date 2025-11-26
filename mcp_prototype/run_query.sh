d3ee72947df53b35b3121d7bbeb4957665542405
“Viru Gajanayake” <viru@not.set>
2025-11-26 20:07:55 +1030

    MCP prototype

--- mcp_prototype/run_query.sh
+++ mcp_prototype/run_query.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/go/bin/mcphost -m openai:gpt-4 -p "examine the data sizes sent to IP address 11.22.33.44 using the get_data_sizes_for_ip_address tool and determine if there is a pattern. do not use mathematical operations such as means to find patterns"  
+~/go/bin/mcphost -m openai:gpt-4 -p "examine the data sizes sent to IP address 11.22.33.44 using the get_data_sizes_for_ip_address tool and determine if there is a pattern. do not use mathematical operations such as means to find patterns" 
--- mcp_prototype/setup.sh
+++ mcp_prototype/setup.sh
@@ -5,4 +5,3 @@ pip3 install fastmcp --break-system-packages
 go install github.com/mark3labs/mcphost@latest
 cp .mcphost.json ~/
 cp mcp_server.py /tmp
-
