7c4e060fded3dd98c2a47b848859b5582848660d
“Viru Gajanayake” <viru@not.set>
2025-11-26 14:06:13 +1030

    MCP prototype

--- mcp_prototype/setup.sh
+++ mcp_prototype/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install python3-pip
+sudo apt  install golang-go
 pip3 install fastmcp --break-system-packages
 go install github.com/mark3labs/mcphost@latest
 cp .mcphost.json ~/
