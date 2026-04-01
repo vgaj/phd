b7562ab3fb7851730236121556a671867ac7a7d1
“Viru Gajanayake” <84622367+vgaj@users.noreply.github.com>
2026-04-01 16:54:35 +1030

    Ensure the correct ui is deployed for dev

--- install_dev.sh
+++ install_dev.sh
@@ -35,7 +35,7 @@ cwd=$(pwd)
 
 echo "*** Linking / Copying files..."
 # This is accessed by a non-root user
-sudo cp -f $cwd/ui/target/phd-ui.jar /usr/share/phone-home-detector/phd-ui.jar
+sudo cp -f $cwd/ui-react/target/phd-ui-react.jar /usr/share/phone-home-detector/phd-ui-react.jar
 sudo ln -f -s $cwd/server/target/phd-server.jar /usr/share/phone-home-detector/phd-server.jar
 sudo ln -f -s $cwd/cli/target/phd-cli-jar-with-dependencies.jar /usr/share/phone-home-detector/phd-cli-jar-with-dependencies.jar
 sudo ln -f -s $cwd/server/src/main/bpf/phone_home_detector_bpf_count.c /usr/share/phone-home-detector/phone_home_detector_bpf_count.c
